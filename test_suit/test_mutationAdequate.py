2e34ffb0aa3745b76c17b7079edab6e713d7ae99
Mahbuba Tasmin <tasmin153@gmail.com>
2023-10-17 07:50:47 -0400

    test files update

--- test_suit/test_mutationAdequate.py
+++ test_suit/test_mutationAdequate.py
@@ -4,228 +4,114 @@ from isTriangle import Triangle
 
 class MutantAdequateTest(unittest.TestCase):
 
-    def test_killMutant1(self):
-        actual = Triangle.classify(1, 1, 1)
-        expected = Triangle.Type.EQUILATERAL
-        self.assertEqual(actual, expected)
+    def test_killMutant1_2_3_15_16_36_38_40(self):
+        self.assertEqual(Triangle.classify(3, 4, 5), Triangle.Type.SCALENE)
+    
+    def test_killMutant4_7_11_17_32_44_45(self):
+        self.assertEqual(Triangle.classify(4, 4, 5), Triangle.Type.ISOSCELES)
 
-    #new    
-    def test_killMutant2(self):
-        actual = Triangle.classify(3, 3, 3)
-        expected = Triangle.Type.EQUILATERAL
-        self.assertEqual(actual, expected)
-    #new  
-    def test_killMutant3(self):
-        actual = Triangle.classify(10, 10, 10)
-        expected = Triangle.Type.EQUILATERAL
-        self.assertEqual(actual, expected)
+    def test_killMutant5_12_18_33_47_48(self):
+        self.assertEqual(Triangle.classify(3, 5, 3), Triangle.Type.ISOSCELES)
+    
+    def test_killMutant6_19_43_50_51(self):
+        self.assertEqual(Triangle.classify(5, 4, 4), Triangle.Type.ISOSCELES)
+    
+    def test_killMutant8_9_10_13_14_26_30_34_35_42(self):
+        self.assertEqual(Triangle.classify(7, 7, 7), Triangle.Type.EQUILATERAL)
 
+    def test_killmutant21_27_28(self):
+        self.assertEqual(Triangle.classify(0, 1, 1), Triangle.Type.INVALID)
+    
+    def test_killMutant29(self):
+        self.assertEqual(Triangle.classify(1, 0, 1), Triangle.Type.INVALID)
 
-    def test_killMutant4(self):
-        actual = Triangle.classify(-1, 10, 10)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-    #new  
-    def test_killMutant5(self):
-        actual = Triangle.classify(10, 10, 20)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-    #new  
-    def test_killMutant6(self):
-        actual = Triangle.classify(10, 20, 10)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-    #new  
-    def test_killMutant7(self):
+    def test_killMutant31(self):
+        self.assertEqual(Triangle.classify(1, 1, 0), Triangle.Type.INVALID)
+        self.assertEqual(Triangle.classify(1, 1, -1), Triangle.Type.INVALID)
+
+    def test_killMutant52(self):
         actual = Triangle.classify(20, 10, 10)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
-    #new
-    def test_killMutant8(self):
-        actual = Triangle.classify(1, 3, 2)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
 
-    def test_killMutant9(self):
-        actual = Triangle.classify(10, -1, 10)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
     #new
-    def test_killMutant10(self):
+    def test_killMutant22_41(self):
         actual = Triangle.classify(3, 2, 1)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-    def test_killMutant11(self):
-        actual = Triangle.classify(pow(2, 31)^-5, pow(2, 31)^-6, 10)
-        expected = Triangle.Type.INVALID
-        print(actual, expected)
-        self.assertEqual(actual, expected)
-    #new
-    def test_killMutant12(self):
-        actual = Triangle.classify(2**31, 2**31, 2**31)
-        expected = Triangle.Type.EQUILATERAL
-        self.assertEqual(actual, expected)
-    #new
-    def test_killMutant13(self):
-        actual = Triangle.classify(3, 4, 7)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-    #new
-    def test_killMutant14(self):
-        actual = Triangle.classify(7, 3, 4)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
 
-
-    def test_killMutant18(self):
-        actual = Triangle.classify(10, 10, -1)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-
-    def test_killMutant20(self):
-        actual = Triangle.classify(pow(2, 31)^-5, 10, pow(2, 31)^-6)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-
-    def test_killMutant59(self):
-        pass  # EQUIVALENT
-
-    def test_killMutant63(self):
+    # #new
+    def test_killMutant37(self):
         actual = Triangle.classify(3, 4, 7)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-    def test_killMutant66(self):
-        actual = Triangle.classify(1, 2, 3)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
 
-    def test_killMutant70(self):
+    def test_killMutant39(self):
         actual = Triangle.classify(4, 7, 3)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-    def test_killMutant73(self):
-        actual = Triangle.classify(1, 3, 2)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-
-    def test_killMutant76(self):
-        actual = Triangle.classify(pow(2, 31)^-2, pow(2, 30), pow(2, 31)^-1)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-
-    def test_killMutant80(self):
-        actual = Triangle.classify(5, 7, 6)
-        expected = Triangle.Type.SCALENE
-        self.assertEqual(actual, expected)
-
-    def test_killMutant81(self):
-        actual = Triangle.classify(7, 3, 4)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-
-    def test_killMutant84(self):
-        actual = Triangle.classify(3, 2, 1)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-
-    def test_killMutant87(self):
-        actual = Triangle.classify(pow(2, 30), pow(2, 30)^-1, pow(2, 31)^-2)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-
-    def test_killMutant101(self):
-        pass  # EQUIVALENT
-
-    def test_killMutant105(self):
+    def test_killMutant23_24_25_46(self):
         actual = Triangle.classify(10, 10, 20)
         expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-        
-    def test_killMutant111(self):
-        a = 1 << 31 - 1
-        b = 1 << 31 - 1
-        c = 1 << 31
-        actual = Triangle.classify(a, b, c)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
+        self.assertEqual(actual, expected)     
 
-    def test_killMutant122(self):
+    def test_killMutant49(self):
         actual = Triangle.classify(10, 20, 10)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-    def test_killMutant125(self):
-        actual = Triangle.classify(10, 21, 10)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
 
-    def test_killMutant128(self):
-        a = 1 << 31 
-        b = 1 << 31 - 1
-        c = 1 << 31 - 1
-        actual = Triangle.classify(a, b, c)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
 
-    def test_killMutant136(self):
-        # EQUIVALENT
-        pass
+    # # Addressing Mutant 32
+    # def test_killMutant32(self):
+    #     self.assertEqual(Triangle.classify(2, 2, 3), Triangle.Type.ISOSCELES)
+    #     self.assertEqual(Triangle.classify(2, 3, 2), Triangle.Type.ISOSCELES)
+    #     self.assertEqual(Triangle.classify(3, 2, 2), Triangle.Type.ISOSCELES)
 
-    def test_killMutant139(self):
-        actual = Triangle.classify(20, 10, 10)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
+    # def test_classify_with_one_side_zero(self):
+        
+        
+    #     self.assertEqual(Triangle.classify(1, 1, 0), Triangle.Type.INVALID)
 
-    #new
-    def test_classify_invalid_triangles(self):
-        self.assertEqual(Triangle.classify(0, 3, 4), Triangle.Type.INVALID)
-        self.assertEqual(Triangle.classify(5, 0, 4), Triangle.Type.INVALID)
-        self.assertEqual(Triangle.classify(5, 3, 0), Triangle.Type.INVALID)
-        self.assertEqual(Triangle.classify(-2, 3, 4), Triangle.Type.INVALID)
-        self.assertEqual(Triangle.classify(5, -2, 4), Triangle.Type.INVALID)
-        self.assertEqual(Triangle.classify(5, 3, -2), Triangle.Type.INVALID)
-        self.assertEqual(Triangle.classify(0, 0, 0), Triangle.Type.INVALID)
-        self.assertEqual(Triangle.classify(-1, -1, -1), Triangle.Type.INVALID)
+    # def test_classify_with_one_side_negative(self):
+    #     self.assertEqual(Triangle.classify(-1, 1, 1), Triangle.Type.INVALID)
+    #     self.assertEqual(Triangle.classify(1, -1, 1), Triangle.Type.INVALID)
+    #     self.assertEqual(Triangle.classify(1, 1, -1), Triangle.Type.INVALID)
 
-        # # Test cases with large values using pow() function
-        # self.assertEqual(Triangle.classify(pow(2, 31) - 1, pow(2, 31) - 1, pow(2, 31) - 1), Triangle.Type.INVALID)
-        # self.assertEqual(Triangle.classify(pow(2, 31), pow(2, 31), pow(2, 31) - 1), Triangle.Type.INVALID)
-        # self.assertEqual(Triangle.classify(pow(2, 31) - 1, pow(2, 31), pow(2, 31)), Triangle.Type.INVALID)
-    #new
-    def test_classify_scalene_triangles(self):
-        self.assertEqual(Triangle.classify(5, 7, 9), Triangle.Type.SCALENE)
-        self.assertEqual(Triangle.classify(8, 6, 10), Triangle.Type.SCALENE)
-        self.assertEqual(Triangle.classify(3, 4, 5), Triangle.Type.SCALENE)
-    #new
-    def test_classify_equilateral_triangles(self):
-        self.assertEqual(Triangle.classify(5, 5, 5), Triangle.Type.EQUILATERAL)
-        self.assertEqual(Triangle.classify(2, 2, 2), Triangle.Type.EQUILATERAL)
-    #new
-    def test_classify_isosceles_triangles(self):
-        self.assertEqual(Triangle.classify(4, 4, 5), Triangle.Type.ISOSCELES)
-        self.assertEqual(Triangle.classify(3, 5, 3), Triangle.Type.ISOSCELES)
-        self.assertEqual(Triangle.classify(5, 4, 4), Triangle.Type.ISOSCELES)
-    #new
-    def test_classify_additional_triangles(self):
-        # Additional test cases to increase code coverage
-        self.assertEqual(Triangle.classify(3, 4, 5), Triangle.Type.SCALENE)
-        self.assertEqual(Triangle.classify(6, 8, 10), Triangle.Type.SCALENE)
-        self.assertEqual(Triangle.classify(12, 15, 17), Triangle.Type.SCALENE)
 
-        self.assertEqual(Triangle.classify(7, 7, 7), Triangle.Type.EQUILATERAL)
-        self.assertEqual(Triangle.classify(1, 1, 1), Triangle.Type.EQUILATERAL)
 
-        self.assertEqual(Triangle.classify(4, 4, 5), Triangle.Type.ISOSCELES)
-        self.assertEqual(Triangle.classify(3, 5, 3), Triangle.Type.ISOSCELES)
-        self.assertEqual(Triangle.classify(5, 4, 4), Triangle.Type.ISOSCELES)
 
-        self.assertEqual(Triangle.classify(2**31, 2**31, 2**31), Triangle.Type.EQUILATERAL)
-        self.assertEqual(Triangle.classify(2**31, 2**31 - 1, 2**31 - 1), Triangle.Type.ISOSCELES)
-        self.assertEqual(Triangle.classify(2**31 - 1, 2**31 - 1, 2**31), Triangle.Type.ISOSCELES)
-
+    # def test_classify_scalene_triangles(self):
+        
+    #     self.assertEqual(Triangle.classify(6, 8, 10), Triangle.Type.SCALENE)
+    #     self.assertEqual(Triangle.classify(12, 15, 17), Triangle.Type.SCALENE)
+    
+    # def test_classify_equilateral_triangles(self):
+        
+    #     self.assertEqual(Triangle.classify(1, 1, 1), Triangle.Type.EQUILATERAL)
+    #     self.assertEqual(Triangle.classify(2**31, 2**31, 2**31), Triangle.Type.EQUILATERAL)
+    
+    # def test_classify_isosceles_triangles(self):
+        
+    #     self.assertEqual(Triangle.classify(2**31, 2**31 - 1, 2**31 - 1), Triangle.Type.ISOSCELES)
+    #     self.assertEqual(Triangle.classify(2**31 - 1, 2**31 - 1, 2**31), Triangle.Type.ISOSCELES)
+    
+    # def test_classify_invalid_triangles(self):
+    #     self.assertEqual(Triangle.classify(0, 3, 4), Triangle.Type.INVALID)
+    #     self.assertEqual(Triangle.classify(5, 0, 4), Triangle.Type.INVALID)
+    #     self.assertEqual(Triangle.classify(5, 3, 0), Triangle.Type.INVALID)
+    #     self.assertEqual(Triangle.classify(-2, 3, 4), Triangle.Type.INVALID)
+    #     self.assertEqual(Triangle.classify(5, -2, 4), Triangle.Type.INVALID)
+    #     self.assertEqual(Triangle.classify(5, 3, -2), Triangle.Type.INVALID)
+    #     self.assertEqual(Triangle.classify(0, 0, 0), Triangle.Type.INVALID)
+    #     self.assertEqual(Triangle.classify(-1, -1, -1), Triangle.Type.INVALID)
+    #     # New test cases to kill the second mutant
+    #     self.assertEqual(Triangle.classify(0, 1, 1), Triangle.Type.INVALID)
+    #     self.assertEqual(Triangle.classify(1, 0, 1), Triangle.Type.INVALID)
+    #     self.assertEqual(Triangle.classify(1, 1, 0), Triangle.Type.INVALID)
 
 
 
--- test_suit/test_triangle.py
+++ test_suit/test_triangle.py
@@ -1,5 +1,8 @@
 import unittest
-from triangle import Triangle
+from isTriangle import Triangle
+# import sys
+# sys.path.append("..")
+# from src.Triangle import Triangle
 
 class TriangleTest(unittest.TestCase):
     def test1(self):
