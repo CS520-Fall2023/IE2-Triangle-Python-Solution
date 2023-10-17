7817d1609b4cc5271fc5bd09b731676b088f87b3
Mahbuba Tasmin <tasmin153@gmail.com>
2023-10-17 07:49:37 -0400

    shell scripts update

--- condition_coverage.sh
+++ condition_coverage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Change to the directory containing the Python script
+## run condition/branch coverage
 cd "./test_suit"
 coverage run --branch test_conditionCoverage.py
 coverage report -m --include="test_conditionCoverage.py"
-coverage html --skip-covered
+coverage html -d coverage_html
--- statement_coverage.sh
+++ statement_coverage.sh
@@ -2,6 +2,5 @@
 # Change to the directory containing the Python script
 cd "./test_suit"
 coverage run -m pytest test_statementCoverage.py
-# coverage json -m --include="test_statementCoverage.py" --pretty-print -o 'coverage.json'
 coverage report -m --include="test_statementCoverage.py"
-coverage html 
+coverage html -d statement_html
